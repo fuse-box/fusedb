6d2024d0f370a8a236791a10dce143f6ed5262a2
cdiaz <me@cristiamdiaz.com>
2017-11-10 08:39:18 -0500

    [fix] clean console.log

--- src/Validator.ts
+++ src/Validator.ts
@@ -6,7 +6,7 @@ export function Validator() {
         let name: string = Target.name;
         name = name.charAt(0).toLowerCase() + name.slice(1)
         name = name.replace('Validator', '');
-        console.log("set", name);
+        //console.log("set", name);
         Schema.validators.set(name, new Target());
     }
 }
